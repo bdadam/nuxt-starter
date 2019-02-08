97d8ec1e3a75c7ec07dcfb8d1d84c9c261d2ccf4
bdadam <me@bdadam.com>
2019-02-08 01:48:12 +0100

    Basic nuxt configuration and first page

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+export default {
+    srcDir: 'src/',
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+    },
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="test">Hello {{ world }}!</h1>
+</template>
+<script>
+export default {
+    data() {
+        return { world: 'World' };
+    },
+};
+</script>
