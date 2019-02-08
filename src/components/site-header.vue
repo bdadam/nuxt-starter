b080875918bb08d4ec4429580a42e84872b2e3ca
bdadam <me@bdadam.com>
2019-02-08 03:00:57 +0100

    Example component (site-header)

---
+++ src/components/site-header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="site-header">Hello</header>
+</template>
+<style lang="scss">
+.site-header {
+    border-bottom: 1px solid #cdcdcd;
+    padding: 24px 0;
+    font-size: 1.25rem;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-    <h1 class="test">Hello {{ world }}!</h1>
+    <div>
+        <site-header/>
+        <h1 class="test">Hello {{ world }}!</h1>
+    </div>
 </template>
 <script>
+import SiteHeader from '~/components/site-header.vue';
+
 export default {
     data() {
         return { world: 'World' };
     },
+    components: { SiteHeader },
 };
 </script>
 <style lang="scss">
